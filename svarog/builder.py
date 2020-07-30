d27991cba5529025886063777083bc40668615d2
Damian Świstowski <damian@swistowski.org>
2020-07-30 14:50:09 +0100

    build none

--- svarog/builder.py
+++ svarog/builder.py
@@ -1,10 +1,34 @@
+from functools import singledispatch
 from typing import Any
-from typing import Callable
+from typing import Type
 from typing import TypeVar
 
 T = TypeVar("T")
 
 
+class CannotDispatch(Exception):
+    pass
+
+
+def sentry(type_: Type, data: Any) -> None:
+    raise CannotDispatch()
+
+
+def none_converter(type_: Type, data: Any) -> None:
+    return None
+
+
+NoneType = type(None)
+
+
 class Builder:
-    def build(self, type: Callable, data: Any) -> T:
-        return type(data)
+    def __init__(self):
+        self.single_dispatch = singledispatch(sentry)
+
+        self.single_dispatch.register(NoneType)(none_converter)
+
+    def build(self, type: Type[T], data: Any) -> T:
+        try:
+            return self.single_dispatch(type, data)  # type: ignore
+        except CannotDispatch:
+            return type(data)  # type: ignore
--- tests/test_builder.py
+++ tests/test_builder.py
@@ -4,3 +4,7 @@ def test_can_build_primitive_str(build):
 
 def test_can_build_primitive_int(build):
     assert build(int, "3") == 3
+
+
+def test_can_build_none(build):
+    assert build(None, None) is None
